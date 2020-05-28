18cc2bfc9556e15162409ebfcca125267bcc88d8
tracht <marija.trachtenberg@gmail.com>
2020-05-28 22:36:43 +0100

    move user data to user.json, add README

---
+++ README.md
@@ -0,0 +1,6 @@
+## Practicing JSON
+1. Install Node from commandline "brew install node"
+2. Connect to a server with commandline "npm install -g live-server"
+3. In the commandline, run "live-server"
+4. Unfortunately, demo from medium article doesn't work!
+https://medium.com/@timothyrobards/understanding-json-in-javascript-5098876d0915 
--- src/example.js
+++ src/example.js
@@ -1,36 +1,34 @@
-let contacts = {
-  name: "Timothy",
-  age: 35,
-  address: {
-    street: "1 Main St",
-    city: "Montreal"
-  },
-  interests: ["cooking", "biking"]
-}
+// let contacts = {
+//   name: "Timothy",
+//   age: 35,
+//   address: {
+//     street: "1 Main St",
+//     city: "Montreal"
+//   },
+//   interests: ["cooking", "biking"]
+// }
 
-// let s = JSON.stringify(contacts);
-// let t = JSON.parse(contacts);
+// let request = new XMLHttpRequest();
+// request.open('GET', 'users.json', true);
+//   reqeust.onload = function () {
+//   // Convert JSON data to an object
+//   let users = JSON.parse(this.response);
+//
+//   let output = '';
+//   for (var i = 0; i < users.length; i++) {
+//     output += '<li>' + users[i].name +' is aged ' + users[i].age + ' years.'; '</li>'
+//   }
+//   document.getElementById('users').innerHTML = output;
+// }
+//
+// request.send();
 
-let users = [
-  {
-    name: "Timothy",
-    age: 35
-  },
-  {
-    name: "Georgette",
-    age: 29
-  },
-  {
-    name: "Craig",
-    age: 54
-  }
-];
-
-let output = '';
-
-for (var i = 0; i < users.length; i++) {
-  // console.log(users[i].name);
-  output += '<li>'+users[i].name+'</li>';
-}
-
-document.getElementById('users').innerHTML = output;
+fetch('./users.json').then(response => {
+  return response.json();
+}).then(data => {
+  //WORK WITH YOUR JSON DATA HERE
+  console.log(data);
+}).catch(err => {
+  //WHAT TO DO WHEN REQUEST FAILS
+  console.log('request failed');
+});
